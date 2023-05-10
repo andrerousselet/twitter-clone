f0226396e4cce9b2881e6969a15561bc39500455
Andre Rousselet <andre098@hotmail.com>
2023-05-10 19:57:20 -0300

    feat(new tweet form): add NewTweetForm component with textarea and tweet button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,18 @@
 import { type NextPage } from "next";
 import { signIn, signOut, useSession } from "next-auth/react";
+import { NewTweetForm } from "~/components/NewTweetForm";
 
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  return <h1>Hi</h1>;
+  return (
+    <>
+      <header className="sticky top-0 z-10 border-b pt-2 bg-white">
+        <h1 className="mb-2 px-4 text-lg font-bold">Home</h1>
+      </header>
+      <NewTweetForm />
+    </>
+  );
 };
 
 export default Home;
