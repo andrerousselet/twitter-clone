bffe7dddbd5c3818ecf82fce4630e3104f8551a2
Andre Rousselet <andre098@hotmail.com>
2023-05-11 12:09:58 -0300

    fix(new tweet form): return null if session.status not authenticated

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -5,13 +5,14 @@ import { ProfileImage } from "./ProfileImage";
 export function NewTweetForm() {
   const session = useSession();
 
-  if (session.status !== "authenticated") return;
+  if (session.status !== "authenticated") return null;
 
   return (
     <form className="flex flex-col gap-2 border-b px-4 py-2">
       <div className="flex gap-4">
         <ProfileImage src={session.data.user.image} />
         <textarea
+          style={{ height: 0 }}
           className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none"
           placeholder="What's happening?"
         />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ import { api } from "~/utils/api";
 const Home: NextPage = () => {
   return (
     <>
-      <header className="sticky top-0 z-10 border-b pt-2 bg-white">
+      <header className="sticky top-0 z-10 border-b bg-white pt-2">
         <h1 className="mb-2 px-4 text-lg font-bold">Home</h1>
       </header>
       <NewTweetForm />
